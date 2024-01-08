681392c4fe4f95149fd93ad5d399fb0c4b3873b2
Sebastian <sebastian_larocca@hotmail.com>
2024-01-08 00:16:23 -0300

    Eliminado console log

--- app.js
+++ app.js
@@ -7,16 +7,14 @@ app.get('/products', async (req, res) => {
     if (req.query.limit) {
         let prods = await productManager.getProducts();
         let lista = JSON.parse(prods)
-
-        console.log('hay query params')
-        res.send(lista.slice(0,req.query.limit))
+        res.send(lista.slice(0, req.query.limit))
     } else {
         let prods = await productManager.getProducts();
         res.send(prods)
     }
 })
 
-app.get('/products/:pid', async (req, res)=>{
+app.get('/products/:pid', async (req, res) => {
     let prod = await productManager.getProductsById(req.params.pid);
     res.send(prod)
 })
